f86264711cf56deb5f341e46ff9b286d14a0966c
TheJudgeMonkey <mr.stas.anufriev@gmail.com>
2021-08-06 20:22:56 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useReducer } from 'react';
+import Routes from './routes';
+import Context from './utils/context';
+import * as ACTIONS from './store/actions/actions';
+import * as Reducer1 from './store/hooks_state/reducer1_hooks';
+import * as UserReducer from './store/hooks_state/user_reducer_hooks';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  const [valueGlobal, setValueGlobal] = useState(0)
+
+  const [stateReducer1, dispatchReducer1] = useReducer(Reducer1.Reducer1, Reducer1.initialState)
+  const [stateContext, dispatchContext] = useReducer(UserReducer.UserReducer, UserReducer.initialState)
+
+  const incrementValueGlobal = () => {
+    setValueGlobal(valueGlobal + 1 )
+  }
+
+  const decrementValueGlobal = () => {
+    setValueGlobal(valueGlobal - 1 )
+  }
+
+  const handleDispatchContextTrue = () => {
+    dispatchReducer1(ACTIONS.success())
+  }
+
+  const handleDispatchContextFalse = () => {
+    dispatchReducer1(ACTIONS.failure())
+  }
+
+  const handleuseContextChange = (event) => {
+    dispatchContext(ACTIONS.user_input_change(event.target.value))
+  }
+
+  const handleuseContextSubmit = (event) => {
+    event.preventDefault();
+    event.persist();
+    dispatchContext(ACTIONS.user_input_submit(event.target.useContext.value))
+  }
+
+  return(
+    <div>
+      React
+      <Context.Provider
+        value={{
+          userReducer1State: stateReducer1.stateprop2,
+          dispatchContextTrue: () => handleDispatchContextTrue(),
+          dispatchContextFalse: () => handleDispatchContextFalse(),
+
+          valueGlobalState: valueGlobal,
+          addGlobalValue: () => incrementValueGlobal(),
+          decGlobalValue: () => decrementValueGlobal(),
+
+          useContextChangeState: stateContext.user_textChange,
+          useContextSubmitState: stateContext.user_textSubmit,
+          useContextSubmit: (event) => handleuseContextSubmit(event),
+          useContextChange: (event) => handleuseContextChange(event)
+          }}>
+        <Routes />
+      </Context.Provider>
     </div>
-  );
+  )
 }
 
 export default App;
